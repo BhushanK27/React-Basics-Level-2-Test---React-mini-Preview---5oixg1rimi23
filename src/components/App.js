96a1cffa5064ff31946a1b687659360499ef38d0
Bhushan Kulkarni <bkkulkarni39@gmail.com>
2022-04-16 14:36:42 +0530

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,48 @@
-import React,{useState} from 'react'
-// import { useState } from 'react/cjs/react.production.min';
+import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 import { Preview } from './Preview';
 const App = () => {
 const [text, setText] = useState("hello world")
 const[font,setFont]=useState(10);
 const[padding,setPadding]=useState(5);
+
+const[fontSize,setFontSize]=useState({fontSize:`${font}px`})
+const[pad,setPad]=useState({padding:`${padding}px`});
+
+function handleInput(event)
+{
+  if (event.target.id === 'contentInput')
+  {
+    setText(event.target.value);
+  }
+  else if(event.target.id ==='fontSizeInput')
+  {
+    setFont(event.target.value);
+    
+  }
+  else if(event.target.id === 'paddingInput') 
+  {
+    setPadding(event.target.value);
+    
+  }
+}
+
+useEffect(() => {
+  setFontSize(
+    { fontSize: `${font}px` }
+  )
+  setPad(
+    { padding: `${padding}px` }
+  )
+  
+}, [font,padding])
+
+
   return (
     <div id="main">
       <div>
         {/* add input elememnts inside here */}
-        <label htmlFor='contentInput'>Content</label>
+        {/* <label htmlFor='contentInput'>Content</label>
         <input type={'text'} value={text} id='contentInput' onChange={(e)=> setText(e.target.value)}></input>
         <br/>
         <label htmlFor='paddingInput'>Padding</label>
@@ -18,11 +50,20 @@ const[padding,setPadding]=useState(5);
         <br/>
         <label htmlFor='fontSizeInput'>Font Size</label>
         <input type={'number'} value={font} id='fontSizeInput' onChange={(e) => setFont(e.target.value)}></input>
-        <br/>
+        <br/> */}
+        <label htmlFor='contentInput'>Content</label>
+        <input type={'text'} value={text} id='contentInput' onChange={(event) => handleInput(event)}></input>
+        <br />
+        <label htmlFor='paddingInput'>Padding</label>
+        <input type={'number'} value={padding} id='paddingInput' onChange={(event) => handleInput(event)}></input>
+        <br />
+        <label htmlFor='fontSizeInput'>Font Size</label>
+        <input type={'number'} value={font} id='fontSizeInput' onChange={(event) => handleInput(event)}></input>
+        <br />
       </div>
  
  {/* render Preview component here */}
-      <Preview fontSize={font} content={text} padding={padding} />
+      <Preview fontSize={fontSize.fontSize} content={text} padding={pad.padding} />
     </div>
   )
 }
--- src/components/Preview.js
+++ src/components/Preview.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 export const Preview = ({fontSize,content,padding}) =>{
     return (
-        <div id="preview" style={{padding:`${padding}px`,fontSize:`${fontSize}px`}}>
+        <div id="preview" style={{padding,fontSize}}>
+            {/* <div id="preview" style={{ padding: `${padding}px`, fontSize: `${fontSize}px` }}> */}
+        {/* {console.log(fontSize)}
+        {console.log(padding)} */}
             {content}
         </div>
     )
